e05fdd23effe16c1178ccabeba60e1c885bfab91
Dimuch_ <antibog8@gmail.com>
2017-09-02 22:53:35 +0300

    ViewPager change on RecycleView modify ActionBar

--- app/src/main/java/com/example/dimuch/task1_primitivelayout/MainActivity.java
+++ app/src/main/java/com/example/dimuch/task1_primitivelayout/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.dimuch.task1_primitivelayout;
 
 import android.content.Intent;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.StringRes;
 import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitleAppBar(R.string.application_name);
+        setIconAppBar(R.drawable.vk_logo);
 
         ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.main_activity);
         layout.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
@@ -31,4 +38,20 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, iIdPerson);
         startActivity(intent);
     }
+
+    public void setTitleAppBar(@StringRes int resId) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(resId);
+        }
+    }
+
+    public void setIconAppBar(@DrawableRes int resId) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setIcon(resId);
+        }
+    }
+
 }
--- app/src/main/java/com/example/dimuch/task1_primitivelayout/PrimitiveLayout.java
+++ app/src/main/java/com/example/dimuch/task1_primitivelayout/PrimitiveLayout.java
@@ -1,49 +1,117 @@
 package com.example.dimuch.task1_primitivelayout;
 
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.StringRes;
 import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PrimitiveLayout extends AppCompatActivity{
+
+    private RecyclerView rvNews;
+    private RecyclerView rvPhotos;
+    private RVAdapterForNews rvAdapterForAllNews;
+    private RVAdapterForNews rvAdapterForPersonalNews;
+    private RVAdapterForPhotos rvAdapterForPhotos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test_layout);
+        setContentView(R.layout.activity_primitive_layout);
+
+        setTitleAppBar(R.string.profile);
+        setIconAppBar(R.drawable.vk_logo);
+        setIconArrowBackAppBar(R.drawable.arrow_left);
 
         int iIdPerson = getIntent().getIntExtra(MainActivity.EXTRA_MESSAGE, -1);
 
         PersonProfile profile = new PersonProfile();
         fillViews(profile);
 
-        RecyclerView rvPhotos = (RecyclerView) findViewById(R.id.rvPhotos);
-        LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
-        rvPhotos.setLayoutManager(llm);
-        RVAdapterForPhotos adapter = new RVAdapterForPhotos(profile.getlIdPhotos());
-        rvPhotos.setAdapter(adapter);
+        rvPhotos = (RecyclerView) findViewById(R.id.rvPhotos);
+        LinearLayoutManager layoutManagerForPhotos = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
+        rvPhotos.setLayoutManager(layoutManagerForPhotos);
+        rvAdapterForPhotos = new RVAdapterForPhotos(profile.getlIdPhotos());
+        rvPhotos.setAdapter(rvAdapterForPhotos);
 
+        rvNews = (RecyclerView) findViewById(R.id.rvNews);
+        LinearLayoutManager layoutManagerForNews = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        rvNews.setLayoutManager(layoutManagerForNews);
+        rvAdapterForAllNews = new RVAdapterForNews(sendNewsPhoto(), sendNewsName(), sendNewsDate(), sendNewsNews());
+        rvAdapterForPersonalNews = new RVAdapterForNews(sendNewsPhoto1(), sendNewsName1(), sendNewsDate1(), sendNewsNews1());
+        rvNews.setAdapter(rvAdapterForAllNews);
 
-
-        ViewPager viewPager = (ViewPager) findViewById(R.id.vpNewsFeed);
-        setupViewPager(viewPager);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tlNewsFeed);
-        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+            }
 
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+                switch (tab.getPosition()) {
+                    case 1:
+                        Toast.makeText(getApplicationContext(), "0", Toast.LENGTH_SHORT).show();
+                        rvNews.setAdapter(rvAdapterForAllNews);
+                        break;
+                    case 0:
+                        Toast.makeText(getApplicationContext(), "1", Toast.LENGTH_SHORT).show();
+                        rvNews.setAdapter(rvAdapterForPersonalNews);
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
 
     }
 
-    private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new AllNewsFragment(), "Все записи");
-        adapter.addFragment(new PersonNewsFragment(), "Записи Элвиса");
-        viewPager.setAdapter(adapter);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return true;
+    }
+
+    public void setTitleAppBar(@StringRes int resId) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(resId);
+        }
+    }
+
+    public void setIconArrowBackAppBar(@DrawableRes int resId) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(resId);
+        }
+    }
+
+    public void setIconAppBar(@DrawableRes int resId) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setIcon(resId);
+        }
+    }
+
+    public void setIconMenuAppBar(@DrawableRes int resId) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+        }
     }
 
     private void fillViews(PersonProfile profile) {
@@ -80,9 +148,103 @@ public class PrimitiveLayout extends AppCompatActivity{
         tvAudios.setText(profile.getsPersonAudios());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_profile, menu);
-        return true;
+    public List<Integer> sendNewsPhoto() {
+        List<Integer> lIdNewsPhotos = new ArrayList<>();
+        lIdNewsPhotos.add(R.drawable.photo);
+        lIdNewsPhotos.add(R.drawable.photo);
+        lIdNewsPhotos.add(R.drawable.photo);
+        lIdNewsPhotos.add(R.drawable.photo);
+        lIdNewsPhotos.add(R.drawable.photo);
+        lIdNewsPhotos.add(R.drawable.photo);
+        lIdNewsPhotos.add(R.drawable.photo);
+        lIdNewsPhotos.add(R.drawable.photo);
+        lIdNewsPhotos.add(R.drawable.photo);
+        return lIdNewsPhotos;
+    }
+
+    public List<String> sendNewsName() {
+        List<String> lNewsName = new ArrayList<>();
+        lNewsName.add("Элвис Пресли");
+        lNewsName.add("Элвис Пресли");
+        lNewsName.add("Элвис Пресли");
+        lNewsName.add("Элвис Пресли");
+        lNewsName.add("Элвис Пресли");
+        lNewsName.add("Элвис Пресли");
+        lNewsName.add("Элвис Пресли");
+        lNewsName.add("Элвис Пресли");
+        lNewsName.add("Элвис Пресли");
+        return lNewsName;
+    }
+
+    public List<String> sendNewsDate() {
+        List<String> lNewsDate = new ArrayList<>();
+        lNewsDate.add("26 июня 1977");
+        lNewsDate.add("26 июня 1977");
+        lNewsDate.add("26 июня 1977");
+        lNewsDate.add("26 июня 1977");
+        lNewsDate.add("26 июня 1977");
+        lNewsDate.add("26 июня 1977");
+        lNewsDate.add("26 июня 1977");
+        lNewsDate.add("26 июня 1977");
+        lNewsDate.add("26 июня 1977");
+        return lNewsDate;
+    }
+
+    public List<String> sendNewsNews() {
+        List<String> lIdNewsNews = new ArrayList<>();
+        lIdNewsNews.add("Hi! You are loh :D");
+        lIdNewsNews.add("Hi! You are loh :D");
+        lIdNewsNews.add("Hi! You are loh :D");
+        lIdNewsNews.add("Hi! You are loh :D");
+        lIdNewsNews.add("Hi! You are loh :D");
+        lIdNewsNews.add("Hi! You are loh :D");
+        lIdNewsNews.add("Hi! You are loh :D");
+        lIdNewsNews.add("Hi! You are loh :D");
+        lIdNewsNews.add("Hi! You are loh :D");
+        return lIdNewsNews;
+    }
+
+    public List<Integer> sendNewsPhoto1() {
+        List<Integer> lIdNewsPhotos = new ArrayList<>();
+        lIdNewsPhotos.add(R.mipmap.ic_launcher);
+        lIdNewsPhotos.add(R.mipmap.ic_launcher);
+        lIdNewsPhotos.add(R.mipmap.ic_launcher);
+        lIdNewsPhotos.add(R.mipmap.ic_launcher);
+        lIdNewsPhotos.add(R.mipmap.ic_launcher);
+        lIdNewsPhotos.add(R.mipmap.ic_launcher);
+        return lIdNewsPhotos;
+    }
+
+    public List<String> sendNewsName1() {
+        List<String> lNewsName = new ArrayList<>();
+        lNewsName.add("Элвис Пресли");
+        lNewsName.add("Элвис Пресли");
+        lNewsName.add("Элвис Пресли");
+        lNewsName.add("Элвис Пресли");
+        lNewsName.add("Элвис Пресли");
+        lNewsName.add("Элвис Пресли");
+        return lNewsName;
+    }
+
+    public List<String> sendNewsDate1() {
+        List<String> lNewsDate = new ArrayList<>();
+        lNewsDate.add("26 июня 1977");
+        lNewsDate.add("26 июня 1977");
+        lNewsDate.add("26 июня 1977");
+        lNewsDate.add("26 июня 1977");
+        lNewsDate.add("26 июня 1977");
+        lNewsDate.add("26 июня 1977");
+        return lNewsDate;
+    }
+
+    public List<String> sendNewsNews1() {
+        List<String> lIdNewsNews = new ArrayList<>();
+        lIdNewsNews.add("Hi! You are loh :D");
+        lIdNewsNews.add("Hi! You are loh :D");
+        lIdNewsNews.add("Hi! You are loh :D");
+        lIdNewsNews.add("Hi! You are loh :D");
+        lIdNewsNews.add("Hi! You are loh :D");
+        lIdNewsNews.add("Hi! You are loh :D");
+        return lIdNewsNews;
     }
 }
